a840096ef06b24c84dbf8a73659696556eb03e8d
Titouan Dionet <titouan.dionet@univ-lorraine.fr>
2024-12-03 11:46:42 +0100

    Add new plot.

--- analyses/plot-data.R
+++ analyses/plot-data.R
@@ -9,6 +9,7 @@ library(ggplot2)
 
 pantheria_path = here::here("data", "pantheria", "PanTHERIA_1-0_WR05_Aug2008.txt")
 pantheria_data = fread(file = pantheria_path)
+pantheria_data[pantheria_data == -999] = NA # Replace -999 by NA
 
 ## Plot ----
 
@@ -17,8 +18,12 @@ plot(x = pantheria_data$`5-1_AdultBodyMass_g`, y = pantheria_data$`18-1_BasalMet
 
 # gg plot
 ggplot(pantheria_data) +
-  aes(x = `5-1_AdultBodyMass_g`, y = `18-1_BasalMetRate_mLO2hr`) +
-  geom_point()
+  aes(x = `5-1_AdultBodyMass_g`/1000, y = `18-1_BasalMetRate_mLO2hr`/1000) +
+  coord_cartesian(xlim = c(0, 1000), ylim = c(0, 100)) +
+  geom_point() +
+  labs(x = "Adult body mass (kg)",
+       y = "Basal metabolic rate (µL O2 / hr)") +
+  theme_bw()
 
 # New graph
 ggplot(pantheria_data) +
