e23639d585801a56ca55410e1ce475a5edc0e72f
Titouan Dionet <titouan.dionet@univ-lorraine.fr>
2024-12-04 14:07:44 +0100

    Add new function [plot_ecoregions.R].

---
+++ R/plot_ecoregions.R
@@ -0,0 +1,27 @@
+#' Count ecoregions occurences
+#' 
+#' @description 
+#' This function count the number of ecoregions where species are present.
+#' 
+#' @return This function returns a data.table which species names and number 
+#' of ecoregions (`data.table`).
+#' 
+#' @export
+#' 
+
+count_ecoregions = function(data) {
+  setDT(data)
+  data[, .(occurences = .N), by = sci_name]
+}
+
+plot_ecoregions = function(data) {
+  
+  PLOT = ggplot(data) +
+    aes(x = sci_name, y = occurences) +
+    geom_point() +
+    labs(x = "Species", 
+         y = "Occurences") +
+    theme_bw()
+  
+  return(PLOT)
+}
