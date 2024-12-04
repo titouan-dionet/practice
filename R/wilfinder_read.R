981313ff913e0a8e4a1b16af89236265683566e0
Titouan Dionet <titouan.dionet@univ-lorraine.fr>
2024-12-04 11:06:02 +0100

    Add new function wildfinder_read to read the WildFinder datasets. Add new script [02_read_data.R] to read the WildFinder datasets.

---
+++ R/wilfinder_read.R
@@ -0,0 +1,26 @@
+#' Read Wildfinder datasets
+#' 
+#' @description 
+#' This function read the Wildfinder data files (csv files).
+#' 
+#' @return This function returns a list containing the datasets (`data_frame`).
+#' 
+#' @export
+#' 
+
+wildfinder_read = function() {
+  # Path
+  path = here::here("data", "wildfinder")
+  filenames = c("wildfinder-ecoregions_list.csv", "wildfinder-ecoregions_species.csv", "wildfinder-mammals_list.csv")
+  
+  # Data
+  list_data = list()
+  
+  for (filename in filenames) {
+    dest_file = here::here(path, filename)
+    list_data = c(list_data, 
+                  list(readr::read_csv(file = dest_file)))
+  }
+  
+  return(list_data)
+}
---
+++ analyses/02_read_data.R
@@ -0,0 +1,11 @@
+# Read data
+#
+# This script will read the Wilfinder datasets.
+# 
+# All functions used in the script have been developed for this project
+# and can be found in the folder R/.
+#
+# Titouan Dionet <titouan.dionet@univ-lorraine.fr>
+
+# Read WildFinder datasets
+wildfinder_list = wildfinder_read()
